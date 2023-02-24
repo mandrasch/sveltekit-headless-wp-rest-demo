c6de99c1fc5027dc30fffbc2b0fa752822eea1d7
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2023-02-24 16:53:38 +0100

    chore: further style improvements

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,31 +2,28 @@
 	import { base } from '$app/paths';
 </script>
 
-<nav class="container-fluid">
+<nav class="container">
 	<ul>
 		<li><a href="{base}/"><strong>Example Brand</strong></a></li>
 	</ul>
 	<ul>
-		<li><a href="{base}/blocks-demo">Gutenberg Blocks Demo</a></li>
+		<li><a href="{base}/blocks-demo">Gutenberg Demo Content</a></li>
 		<!-- <li><a href="#">Link</a></li> -->
+
+		<li>
+			<details role="list">
+				<summary aria-haspopup="listbox" role="link" class="contrast">Dropdown</summary>
+				<ul role="listbox">
+					<li><a href="../preview/">This is just a</a></li>
+					<li><a href="../classless/">test to check</a></li>
+					<li><a href="../basic-template/">whether dropdowns</a></li>
+					<li><a href="../company/">are accessible.</a></li>
+				</ul>
+			</details>
+		</li>
 		<li>
 			<a href="https://github.com/mandrasch/sveltekit-headless-wp-rest-demo" role="button">GitHub</a
 			>
-		</li>
-		<li>
-			<details role="list" dir="rtl">
-				<summary aria-haspopup="listbox" role="link" class="contrast">Examples</summary>
-				<ul role="listbox">
-					<li><a href="../preview/">Preview</a></li>
-					<li><a href="../preview-rtl/">Right-to-left</a></li>
-					<li><a href="../classless/">Class-less</a></li>
-					<li><a href="../basic-template/">Basic template</a></li>
-					<li><a href="../company/">Company</a></li>
-					<li><a href="../google-amp/">Google Amp</a></li>
-					<li><a href="../sign-in/">Sign in</a></li>
-					<li><a href="../bootstrap-grid/">Bootstrap grid</a></li>
-				</ul>
-			</details>
 		</li>
 	</ul>
 </nav>
